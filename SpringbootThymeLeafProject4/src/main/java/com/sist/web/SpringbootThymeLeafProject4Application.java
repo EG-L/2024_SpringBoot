2845c4e17209488920f1ad2e6a0dd11aa2dba29d
EG-L <pshowx12@naver.com>
2024-02-28 12:22:03 +0900

    20240228

---
+++ SpringbootThymeLeafProject4/src/main/java/com/sist/web/SpringbootThymeLeafProject4Application.java
@@ -0,0 +1,13 @@
+package com.sist.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThymeLeafProject4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootThymeLeafProject4Application.class, args);
+	}
+
+}
