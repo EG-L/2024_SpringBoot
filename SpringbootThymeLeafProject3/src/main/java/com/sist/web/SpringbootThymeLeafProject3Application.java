bc02836777d5b94b552628c292f34b8251f3d2dd
EG-L <pshowx12@naver.com>
2024-02-28 10:09:26 +0900

    20240228

---
+++ SpringbootThymeLeafProject3/src/main/java/com/sist/web/SpringbootThymeLeafProject3Application.java
@@ -0,0 +1,13 @@
+package com.sist.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThymeLeafProject3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootThymeLeafProject3Application.class, args);
+	}
+
+}
---
+++ SpringbootThymeLeafProject3/src/main/java/com/sist/web/controller/BoardController.java
@@ -0,0 +1,51 @@
+package com.sist.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.*;
+import com.sist.web.entity.*;
+import com.sist.web.service.*;
+
+@Controller
+@RequestMapping("board/")
+public class BoardController {
+	@Autowired
+	private BoardService bSerivce;
+	
+	@GetMapping("list")
+	public String board_list(String page,Model model) {
+		if(page==null)
+			page="1";
+		int curpage = Integer.parseInt(page);
+		int rowSize = 10;
+		int start = (rowSize*curpage)-(rowSize);
+		List<BoardEntity> list = bSerivce.boardListData(start);
+		int totalpage = bSerivce.boardTotalPage();
+		model.addAttribute("curpage",curpage);
+		model.addAttribute("list",list);
+		model.addAttribute("totalpage",totalpage);
+		
+		return "board/list";
+	}
+	
+	@GetMapping("insert")
+	public String board_insert() {
+		return "board/insert";
+	}
+	
+	@PostMapping("insert_ok")
+	public String board_insert_ok(BoardEntity vo) {
+		bSerivce.save(vo);
+		return "redirect:/board/list";
+	}
+	
+	@GetMapping("detail")
+	public String board_detail(int no,Model model) {
+		
+		return "board/detail";
+	}
+}
